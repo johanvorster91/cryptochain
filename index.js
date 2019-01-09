b07117a6dad356640b92853b09756a993ae99211
johan <johan.vorster91@gmail.com>
2019-01-09 17:07:57 +0200

    fix

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const PubSub = require('./app/pubsub');
 const TransactionPool = require('./wallet/transaction-pool')
 const Wallet = require('./wallet');
 const TransactionMiner = require('./app/transaction-miner')
+const isDevelopment = process.env.ENV === 'development';
 const REDIS_URL = isDevelopment ? 'redis://127.0.0.1.6379' : 'redis ://h:p9d3abe1037f988fcb8ee9b1bb418864295c7c5aac8bf130874d1aad4eb2220e1@ec2-18-210-203-66.compute-1.amazonaws.com:11469'
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
-const isDevelopment = process.env.ENV === 'development';
+
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/dist')))
